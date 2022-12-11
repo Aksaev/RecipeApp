3b6c39901d87e9c927ecd22d56aed0f575b30c23
Aksaev <mail-ax@ya.ru>
2022-12-11 19:46:17 +0400

    Initial commit

---
+++ src/test/java/me/aksaev/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
